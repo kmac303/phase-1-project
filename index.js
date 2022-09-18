4149f3b0913a9ad7ff94f8eee1a538aa29ea5670
Kevin McIntosh <kmac303@gmail.com>
2022-09-17 19:36:30 -0600

    commit tree 2206b94ee851032ec56390b2018f8a0dcb86e7ee

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+function loadREST() {
+  fetch('https://api.publicapis.org/entries')
+  .then(function(response){
+    return response.json();
+    console.log(response);
+  })
+  .then(function(entries){
+     let html = "";
+
+     entries.forEach(function(category){
+      html += `
+        <li>
+          <a href="${entries.Category}
+          ">View API</a>
+          ${entries.API}
+        </li>
+        `;
+     })
+   })
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById(`text`).textContent = "MusicBrainz API Info here";
+    document.getElementById(`text`).textContent = "Search for an API by Category Below";
   });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(e)
+  })
+})
+
+  function darkMode() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+  }

c5248edc205a75e62bef81c3e369f202d51d8f21
Kevin McIntosh <kmac303@gmail.com>
2022-09-18 21:12:57 -0600

    having some issues with git commit

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
-function loadREST() {
-  fetch('https://api.publicapis.org/entries')
-  .then(function(response){
-    return response.json();
-    console.log(response);
+//Searches the TV Maze API
+function searchShows(query) {
+  const site = `http://api.tvmaze.com/search/shows?q=${query}`;
+  fetch(site)
+  .then(response => response.json())
+  .then((data) => {
+    const results = data.map(tvShows => tvShows.show.name);
+    displayResults(results);
   })
-  .then(function(entries){
-     let html = "";
-
-     entries.forEach(function(category){
-      html += `
-        <li>
-          <a href="${entries.Category}
-          ">View API</a>
-          ${entries.API}
-        </li>
-        `;
-     })
-   })
 }
 
+//Shows results in a list
+function displayResults(results) {
+  const list = document.getElementById("output");
+  list.innerHTML = "";
+  results.forEach(showName => {
+    const element = document.createElement("li");
+    element.innerText = showName;
+    list.appendChild(element);
+  });
+}  
 
-
+//Shows text above the input field when the DOM is loaded
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById(`text`).textContent = "Search for an API by Category Below";
+    document.getElementById(`text`).textContent = "Search for a TV Show Below";
   });
 
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log(e)
-  })
+//Runs when the "Search" button is clicked
+const form = document.getElementById('test');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const category = document.getElementById('category');
+  searchShows(category.value);
 })
 
-  function darkMode() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+//Logs what is being typed in the console
+const categoryField = document.getElementById('category');
+categoryField.addEventListener("change", (e) => {
+  console.log(e.target.value);
+});
+
+//Function that adds the Dark Mode CSS configuration
+function darkMode() {
+  let element = document.body;
+  element.classList.add("dark-mode");
+}
+
+//Function that removes the Dark Mode CSS configuration (Light Mode is loaded by default)
+function lightMode() {
+  let element = document.body;
+  element.classList.remove("dark-mode");
+}
+
+//prevents the submit button from refreshing the page when clicked
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.querySelector('form').addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     console.log(e.target.value);
+//   })
+// })
